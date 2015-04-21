3ee85a41811af1c7369ebe0791ee58e6b6bc59e3
kforbes <kforbes@nd.edu>
2015-04-21 01:43:20 -0400

    Tried to create/implement the Background class.  Some bugs still need to be worked out, so it is not currently included in the makefile, all the calls to the class in the driver file have been commented out, and all necessary parts are still in the driver

--- project/Background.cpp
+++ project/Background.cpp
@@ -1,14 +1,113 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <cstdlib>
+#include <ctime>
 #include "Background.h"
 using namespace std;
 
 Background::Background():LTexture() {
-
+	difficulty = 50;
+	frameBack = 0;
+	dirTurn = 0;
+	prev = 0;
 }
+
 
 Background::~Background() {
 
 }
 
+
+void Background::turn() {
+	random = rand() % difficulty; 
+
+	switch(random) {
+		case 1: //both turns
+			frameBack = 1;
+			dirTurn = 2;
+			prev = random;
+			break;
+		case 2: //turn left
+			frameBack = 16;
+			dirTurn = 3;
+			prev = random;	
+			break;
+		case 3: //turn right
+			frameBack = 32;
+			dirTurn = 4;
+			prev = random;
+			break;
+		default: //straight
+			dirTurn = 1;
+			prev = random;
+			break;
+	}
+}
+
+
+SDL_Rect* Background::frames() {
+	if (dirTurn == 2) { //both turns
+		if (numTurn < 16) {
+			currentClipBack = &gBackClips[frameBack/4 + 1];
+			++numTurn;
+			dirTurn = 2;
+			++frameBack;
+		}
+		if(frameBack >= 16) {
+			frameBack = 0;
+			numTurn = 0;
+			dirTurn=0;
+		}
+	}
+	else if (dirTurn == 3) { //left turn
+		if (numTurn < 16) {
+			currentClipBack = &gBackClips[frameBack/4 + 1];
+			++numTurn;
+			dirTurn = 3;
+			++frameBack;
+		}
+		if(frameBack >= 32) {
+			frameBack = 0;
+			numTurn = 0;
+			dirTurn=0;
+		}
+	}
+	else if (dirTurn == 4) { //right turn
+		if (numTurn < 16) {
+			currentClipBack = &gBackClips[frameBack/4 + 1];
+			++numTurn;
+			dirTurn = 4;
+			++frameBack;
+		}
+		if(frameBack >= 48) {
+			frameBack = 0;
+			numTurn = 0;
+			dirTurn=0;
+		}
+	}
+	else if (dirTurn == 1) { //straight
+		currentClipBack = &gBackClips[frameBack];
+		numTurn = 0;
+		dirTurn = 0;
+
+	return currentClipBack;
+}
+
+
+bool Background::userTurn() {
+	if ((numTurn == 14) && (dirTurn == 2) && (userTurn==0))
+		quit = true;	
+	else if ((numTurn == 15) && (userTurn != dirTurn))
+		quit = true;
+
+	if (numTurn < 10)
+	userTurn = 0;
+
+	return quit;
+}
+
+
+int getNumTurn() {
+	return numTurn;
+}
--- project/Background.h
+++ project/Background.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <cstdlib>
+#include <ctime>
 #include "LTexture.h"
 using namespace std;
 
@@ -11,8 +13,19 @@ class Background : public LTexture {
 	public:
 		Background();
 		~Background();
+		void turn(); //function replaces switch case that randomly generates turns
+		SDL_Rect* frames(); //determines what frames are shown depending on the results of turn() function
+		bool userTurn();
+		int getNumTurn();
 	private:
-		int direction; // switch case for various background sets
+		int random; //random number generated for turn
+		int difficulty; //used with random number generator, can be decreased to make game harder
+		int frameBack;
+		int dirTurn;
+		int numTurn;
+		int prev;
+		SDL_Rect* currentClipBack;
+		bool quit;
 };
 
 #endif
